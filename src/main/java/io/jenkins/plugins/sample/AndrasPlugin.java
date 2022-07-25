28449bac558709005aea5f8f8f1acb51b0d818c2
Andra Maria Puscas <apuscas@cloudbees.com>
2022-07-25 15:54:12 +0200

    Level 3 - adding a list

--- src/main/java/io/jenkins/plugins/sample/AndrasPlugin.java
+++ src/main/java/io/jenkins/plugins/sample/AndrasPlugin.java
@@ -2,12 +2,18 @@ package io.jenkins.plugins.sample;
 
 import hudson.Extension;
 import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 import hudson.views.ViewsTabBar;
 import hudson.views.ViewsTabBarDescriptor;
+import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class AndrasPlugin extends ViewsTabBar {
 
@@ -36,6 +42,7 @@ public class AndrasPlugin extends ViewsTabBar {
         public void setGlobalDescription(String globalDescription) {
             this.globalDescription = globalDescription;
         }
+
         public AndrasPluginDescriptor() {
             load();
         }
@@ -58,10 +65,20 @@ public class AndrasPlugin extends ViewsTabBar {
         }
 
         public FormValidation doCheckGlobalName(@QueryParameter String globalName) {
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
             if (globalName.matches("[a-zA-Z\\s]+")) {
                 return FormValidation.ok();
             }
             return FormValidation.warning("Name can only contain lowercase and uppercase letters and spaces");
+        }
+
+        public ListBoxModel doFillCategoriesItems() {
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
+            ListBoxModel items = new ListBoxModel();
+            items.add("first item", UUID.randomUUID().toString());
+            items.add("second item", UUID.randomUUID().toString());
+
+            return items;
         }
     }
 }
