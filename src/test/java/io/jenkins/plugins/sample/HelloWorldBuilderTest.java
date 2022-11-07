ebb6a8529e11cb76341d3fc17acffb4bc4ec2c78
ampuscas <apuscas@cloudbees.com>
2022-11-07 15:20:28 +0100

    modif a test

--- src/test/java/io/jenkins/plugins/sample/HelloWorldBuilderTest.java
+++ src/test/java/io/jenkins/plugins/sample/HelloWorldBuilderTest.java
@@ -16,6 +16,7 @@ public class HelloWorldBuilderTest {
     public JenkinsRule jenkins = new JenkinsRule();
 
     final String name = "Bobby";
+    final String name2 = "Bobby";
 
     @Test
     public void testConfigRoundtrip() throws Exception {
