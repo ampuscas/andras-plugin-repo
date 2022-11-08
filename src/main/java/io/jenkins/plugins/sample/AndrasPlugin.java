cbcee0c10ab44c4175533a26b66e4214a56f26a7
ampuscas <apuscas@cloudbees.com>
2022-11-08 15:04:13 +0100

    modif a class and a test

--- src/main/java/io/jenkins/plugins/sample/AndrasPlugin.java
+++ src/main/java/io/jenkins/plugins/sample/AndrasPlugin.java
@@ -23,7 +23,7 @@ public class AndrasPlugin extends ViewsTabBar {
     public AndrasPlugin() {
         super();
     }
-
+//comment fro test
     @Extension
     public static final class AndrasPluginDescriptor extends ViewsTabBarDescriptor {
 
