fc827399e3756d23ee64289222ee330415426f6d
Andra Maria Puscas <102651154+ampuscas@users.noreply.github.com>
2024-04-17 22:00:54 +0200

    Update AndrasPlugin.java

--- src/main/java/io/jenkins/plugins/sample/AndrasPlugin.java
+++ src/main/java/io/jenkins/plugins/sample/AndrasPlugin.java
@@ -23,7 +23,7 @@ public class AndrasPlugin extends ViewsTabBar {
     public AndrasPlugin() {
         super();
     }
-//////////
+////////////
 
     @Extension
     public static final class AndrasPluginDescriptor extends ViewsTabBarDescriptor {
